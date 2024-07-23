1b79b86a24234acfac21beab8b88c6411deb27ba
danyul <115963400+danyu1@users.noreply.github.com>
2024-07-23 15:01:46 -0700

    implement the right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,34 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox';
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Daniel" };
+  const loggedIn = { firstName: "Daniel", lastName: "Hernandez", email: "danielhernandez@uchicago.edu"};
 
   return (
-    <section className = "home">
-      <div className = "home-content">
+    <section className="home">
+      <div className="home-content">
         <header className="home-header">
           {/* Components have their own params called props  */}
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
           <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
+            accounts={[]}
+            totalBanks={2}
+            totalCurrentBalance={1250.35}
           />
-        </header>    
+        </header>
+        RECENT TRANSACTIONS
       </div>
-    </section>
-  )
-}
 
-export default Home
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{ currentBalance : 800.10 },{ currentBalance : 500.69}]} />
+    </section>
+  );
+};
+
+export default Home;
